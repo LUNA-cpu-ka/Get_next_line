1aef0fa6025bc4565d89e3e9ab86d0be3af5beed
saait-si <sajidaaitsimhand1@gmail.com>
2024-05-02 23:55:07 +0100

    Push

---
+++ get_next_line_folder/get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saait-si <saait-si@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 13:49:02 by saait-si          #+#    #+#             */
+/*   Updated: 2024/01/17 06:51:09 by saait-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+# endif
+
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *stash, char *buffer);
+char	*ft_strchr(const char *s, char c);
+
+#endif
